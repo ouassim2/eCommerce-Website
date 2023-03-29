dccb01d9305dcbc6a1e4d103272fab366be9e64d
Ouassim <112521258+ouassim2@users.noreply.github.com>
2023-03-29 16:17:06 -0400

    successfully upgraded all dependencies -bugs

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,13 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import App from "./components/App";
 
 import { CartProvider } from "./components/CartContext";
 // cartprovider to share prop data throughout
-ReactDOM.render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <CartProvider>
       <App />
     </CartProvider>
   </React.StrictMode>,
-  document.getElementById("root")
 );
